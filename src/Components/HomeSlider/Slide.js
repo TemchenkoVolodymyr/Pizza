21606b30982f74ab2d793e137e3f8361d6d80de2
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-12 20:38:19 +0200

    created HomeSlider Component, created carousel for HomePage

---
+++ src/Components/HomeSlider/Slide.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from "react";
+import './Slide.module.scss'
+import style from './Slide.module.scss'
+import {FaRegDotCircle} from "react-icons/fa";
+import {SiThreedotjs} from "react-icons/si";
+
+const Slide = (props) => {
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const {image} = props
+
+  const sliderStyles = {
+    backgroundImage: `url(${image[currentIndex]}`
+  }
+
+  const clickOnDot = (index) => {
+    setCurrentIndex(index)
+  }
+
+  return (
+    <>
+      <div className={style.container}>
+        <div className={style.carousel} style={sliderStyles}>
+          <div className={style.btns}>
+            <button className={style.order}>Order Now</button>
+            <button className={style.view}>View Menu</button>
+          </div>
+          <div className={style.dots}>
+            {image.map((slide, slideIndex) => <div onClick={() => clickOnDot(slideIndex)} key={slideIndex}>
+              <SiThreedotjs
+                className={currentIndex === slideIndex ? style.active : style.dot} style={{fontSize: "50px"}}></SiThreedotjs></div>)}
+          </div>
+        </div>
+      </div>
+
+
+    </>
+  )
+}
+export default Slide
