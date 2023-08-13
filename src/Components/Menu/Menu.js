8a070405f2459d4de12df8bfc4da33bd2b7a0f2b
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-13 10:12:17 +0200

    created pagination to menu items

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from './Menu.module.scss'
 import gb from '../../assets/bg_4.jpg'
 import axios from "axios";
 import {useSelector} from "react-redux";
 import pizzaImage from '../../assets/pizza-1.jpg'
+import MenuItems from "../../Pagination/MenuItems";
+import Pagination from "../../Pagination/Pagination";
 const Menu = () => {
 
   const dataOfPizzas = useSelector((state) => state.dataOfPizzas)
-  const test = () => {
-    return axios.post(`http://localhost:3000/api/v1/pizza`, {
-      name: 'asdasd',
-      description: 'asdasd',
-      shortDescription: "asdasd",
-      price: 44,
-    })
-  }
 
-  console.log(dataOfPizzas)
+  const [currentPage,setCurrentPage] = useState(1)
+  const [dataPerPage,setDataPerPage] = useState(3)
+  const [loading,setLoading] = useState(false)
+
+  useEffect(() => {
+    if(!dataOfPizzas){
+      setLoading(true)
+    }else{
+      setLoading(false)
+    }
+  },[dataOfPizzas])
+
+  // const test = () => {
+  //   return axios.post(`http://localhost:3000/api/v1/pizza`, {
+  //     name: 'asdasd',
+  //     description: 'asdasd',
+  //     shortDescription: "asdasd",
+  //     price: 44,
+  //   })
+  // }
+
+  const indexOfLastMenuItem = currentPage * dataPerPage;
+
+  const indexOfFirstMenuItem = indexOfLastMenuItem - dataPerPage
+
+  const currentMenu = dataOfPizzas.slice(indexOfFirstMenuItem,indexOfLastMenuItem)
+
+  const paginate = (numberPage) => setCurrentPage(numberPage)
+
   return (
     <>
       <div className={style.container} style={{backgroundImage: `url(${gb})`}}>
@@ -27,22 +49,8 @@ const Menu = () => {
           <p>Far far away,behind the word mountains,far fram the countries Vakalia and Cansonantia,there live the blind
             texts</p>
         </div>
-        <div className={style.itemsMenu}>
-          {dataOfPizzas && dataOfPizzas.map(item => <div className={style.wrapperItems}>
-            <div>
-              <img src={pizzaImage} alt="image"/>
-            </div>
-
-            <div className={style.descriptionItem}>
-              <p className={style.name}>{item.name}</p>
-              <p className={style.description}>{item.description}</p>
-              <div className={style.wrapperPrice}>
-                <p className={style.price}><span>$ {item.price}</span></p>
-                <button>Order</button>
-              </div>
-            </div>
-          </div>)}
-        </div>
+        <MenuItems menuData={currentMenu} loading={loading}></MenuItems>
+        <Pagination dataPerPage={dataPerPage} totalItems={dataOfPizzas.length} paginate={paginate}></Pagination>
       </div>
     </>
   )
