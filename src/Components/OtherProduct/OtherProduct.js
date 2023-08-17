9d08ea11e577000d7068bb3acffa168c904bf1bc
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-17 22:18:34 +0200

    fix problems

--- src/Components/OtherProduct/OtherProduct.js
+++ src/Components/OtherProduct/OtherProduct.js
@@ -1,11 +1,29 @@
 import React from "react";
 import style from './OtherProduct.module.scss'
 import bg from '../../assets/about.jpg'
+import {cartDataOrderAC} from "../../Redux/Cart/cartAC";
+import {useDispatch} from "react-redux";
 
 const OtherProduct = (props) => {
 
   const {currentProduct,getStartProduct,getBurgers, getDrinks, getPastas} = props
 
+const dispatch = useDispatch()
+  const setOrderToLocalStorage = (image,price,name,id) => {
+    const orderData = {
+      image,
+      price,
+      name,
+      ingredients:'Tomato,chess',
+      size:'Large',
+      quantity:1,
+      id
+    }
+    const isLocalStorage = JSON.parse(localStorage.getItem('order')) || [];
+    isLocalStorage.push(orderData)
+    localStorage.setItem('order', JSON.stringify(isLocalStorage))
+    dispatch(cartDataOrderAC(isLocalStorage))
+  }
 
   return (
     <>
@@ -33,7 +51,7 @@ const OtherProduct = (props) => {
               </div>
 
               <p className={style.price}>$ {item.price}</p>
-              <button>Add to cart</button>
+              <button onClick={() => setOrderToLocalStorage(item.image,item.price,item.id)}>Add to cart</button>
             </div>)}
           </div>
         </div>
@@ -44,14 +62,3 @@ const OtherProduct = (props) => {
 }
 export default OtherProduct
 
-
-// const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-//
-// useEffect(() => {
-//   function handleResize() {
-//     setWindowWidth(window.innerWidth);
-//   }
-//   window.addEventListener('resize', handleResize);
-//
-//   return () => window.removeEventListener('resize', handleResize);
-// }, []);
